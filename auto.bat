0cea840a7f805468a4ff6a4334ec9454d2ba298a
s7 <s7887132@gmail.com>
2022-11-30 14:35:52 +0530

    14:35:51.70, 30-11-2022

--- auto.bat
+++ auto.bat
@@ -1,5 +1,6 @@
 @ECHO ON
-start "G:\My Drive\Absolute pvt ltd\EVE\bot\eve.py"
+cd bot
+start eve.py
 
 set hour=%time:~0,2%
 set min=%time:~3,2%
@@ -9,9 +10,9 @@ set /a b=%a%+0
 set /a c=%b%+0
 set /a d=%c%+9
 
-set /a q=%d%-2
-set /a "u=%d%-24" 
-set /a p=%u%-2 
+set /a q=%d%-3
+set /a "u=%d%-24"
+set /a p=%u%-3 
 
 set /a e=%d%+0
 
@@ -19,12 +20,12 @@ schtasks /delete /tn s1 /f
 schtasks /delete /tn s2 /f
 schtasks /delete /tn s3 /f
 schtasks /delete /tn s4 /f  
-schtasks /create /sc DAILY /tn s1 /tr "G:\My Drive\Absolute pvt ltd" /st %a%:00:00 /f
-schtasks /create /sc DAILY /tn s1 /tr "G:\My Drive\Absolute pvt ltd" /st 0%a%:00:00 /f
-schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\Absolute pvt ltd" /st %b%:00:00 /f
-schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\Absolute pvt ltd" /st 0%b%:00:00 /f
-schtasks /create /sc DAILY /tn s3 /tr "G:\My Drive\Absolute pvt ltd" /st %c%:00:00 /f
-schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\Absolute pvt ltd" /st 0%c%:00:00 /f
+schtasks /create /sc DAILY /tn s1 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %a%:00:00 /f
+schtasks /create /sc DAILY /tn s1 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%a%:00:00 /f
+schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %b%:00:00 /f
+schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%b%:00:00 /f
+schtasks /create /sc DAILY /tn s3 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %c%:00:00 /f
+schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%c%:00:00 /f
 
 netsh wlan show networks
 netsh wlan connect Starlink
@@ -39,8 +40,7 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "BO darbi" /min "%~f0" %
 
 if %hour% equ %d% (
     if %min% equ 00 (
-        cd walls
-        ECHO E.ps1
+
         timeout /t 3
         taskkill /f /fi "STATUS eq running"       
         cd ..
@@ -54,8 +54,7 @@ if %hour% equ %d% (
 :END1
 if %hour% equ %u% (
     if %min% equ 00 (
-        cd walls
-        ECHO E.ps1
+
         timeout /t 3
         taskkill /f /fi "STATUS eq running" 
         cd ..
@@ -68,23 +67,22 @@ if %hour% equ %u% (
 )
 :END2
 if %d% geq 24 (
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st 0%p%:00:00 /f
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st %p%:00:00 /f
+    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%p%:00:00 /f
+    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %p%:00:00 /f
     goto alt2
 ) else (
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st 0%q%:00:00 /f 
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st %q%:00:00 /f
+    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%q%:00:00 /f 
+    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %q%:00:00 /f
     goto alt1
 )
 
 :alt1
 if %hour% GEQ %q% (
     if %hour% LSS %d% ( 
-        cd walls
-        ECHO D.ps1
-        start firefox https://www.blogger.com/blog/posts/2034391115397402342
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st 0%d%:00:00 /f 
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st %d%:00:00 /f                              
+
+        start firefox https://www.blogger.com/blog/posts/273122595862652461
+        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%d%:00:00 /f 
+        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %d%:00:00 /f                              
     ) ELSE (
         goto b
     )
@@ -95,11 +93,10 @@ if %hour% GEQ %q% (
 :alt2
 if %hour% GEQ %p% (
     if %hour% LSS %u% (
-        cd walls
-        ECHO D.ps1
-        start firefox https://www.blogger.com/blog/posts/2034391115397402342
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st 0%u%:00:00 /f 
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\Absolute pvt ltd" /st %u%:00:00 /f                                   
+
+        start firefox https://www.blogger.com/blog/posts/273122595862652461
+        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%u%:00:00 /f 
+        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %u%:00:00 /f                                   
     ) ELSE (
         goto b
     )
@@ -110,10 +107,7 @@ if %hour% GEQ %p% (
 :b
 if %hour% GEQ %c% (
     if %hour% LSS %d% (
-        cd walls
-        ECHO C.ps1
-        cd ..
-        notepad
+
         push
         exit
     ) ELSE (
@@ -126,10 +120,7 @@ if %hour% GEQ %c% (
 :c
 if %hour% GEQ %b% (
     if %hour% LSS %c% (
-        cd walls
-        ECHO B.ps1
-        cd ..
-        start firefox
+
         push
         exit
     ) ELSE (
@@ -142,8 +133,7 @@ if %hour% GEQ %b% (
 :o
 if %hour% GEQ %a% (
     if %hour% LSS %b% ( 
-        cd walls
-        ECHO A.ps1
+
         push
         exit
     ) ELSE (
