ad5836eb2d55be21ed7e7dfb46a8264ba129976a
s7 <s7887132@gmail.com>
2022-12-01 10:31:52 +0530

    10:31:51.77, 01-12-2022

--- auto.bat
+++ auto.bat
@@ -5,26 +5,26 @@ start eve.py
 set hour=%time:~0,2%
 set min=%time:~3,2%
 
-set /a a=7
+set /a applications=7
 
-set /a b=%a%+6
+set /a a=%applications%+6
 set /a c=%b%+6
 set /a d=%c%+6
 
-set /a l=3
+set /a rtfms=8
 
 if %d% geq 24 (
     set /a "d=%d%-24"
 ) else ( 
-    goto ok22
+    goto ok1
 ) 
-:ok22
-if %l% geq %d% (
-    set /a q=%d%+24-%l%
+:ok1
+if %rtfms% geq %d% (
+    set /a m=%d%+24-%rtfms%
 ) else ( 
-    set /a p=%d%-%l%
+    set /a m=%d%-%rtfms%
 ) 
-:ok222
+
 schtasks /delete /tn s1 /f
 schtasks /delete /tn s2 /f
 schtasks /delete /tn s3 /f
@@ -35,6 +35,10 @@ schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.ba
 schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%b%:00:00 /f
 schtasks /create /sc DAILY /tn s3 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %c%:00:00 /f
 schtasks /create /sc DAILY /tn s2 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%c%:00:00 /f
+schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %d%:00:00 /f
+schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%d%:00:00 /f
+schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %m%:00:00 /f
+schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%m%:00:00 /f
 
 netsh wlan show networks
 netsh wlan connect Starlink
@@ -43,9 +47,9 @@ del C:\Users\pc14\AppData\Local\Temp /f /q
 rmdir C:\Users\pc14\AppData\Local\Temp /s /q 
 cd %cd%\eve\walls
 
-goto ok
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "BO darbi" /min "%~f0" %* && exit
-:ok
+goto ok2
+
+:ok2
 
 if %hour% equ %d% (
     if %min% equ 00 (
@@ -55,102 +59,63 @@ if %hour% equ %d% (
         cd ..
         push 
     ) else (
-        goto END1
+        goto ok3
     )
 ) else (
-    goto END1
+    goto ok3
 )
-:END1
-if %hour% equ %u% (
-    if %min% equ 00 (
-
-        timeout /t 3
-        taskkill /f /fi "STATUS eq running" 
-        cd ..
-        push  
-    ) else (
-        goto END2   
-    )
-) else (
-    goto END2
-)
-:END2
-if %d% geq 24 (
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%p%:00:00 /f
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %p%:00:00 /f
-    goto alt2
-) else (
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%q%:00:00 /f 
-    schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %q%:00:00 /f
-    goto alt1
-)
-
-:alt1
-if %hour% GEQ %q% (
+:ok3
+if %hour% GEQ %m% (
     if %hour% LSS %d% ( 
 
         start firefox https://www.blogger.com/blog/posts/273122595862652461
         schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%d%:00:00 /f 
         schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %d%:00:00 /f                              
     ) ELSE (
-        goto b
+        goto ok4
     )
 ) ELSE (
-    goto b
+    goto ok4
 )
 
-:alt2
-if %hour% GEQ %p% (
-    if %hour% LSS %u% (
-
-        start firefox https://www.blogger.com/blog/posts/273122595862652461
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st 0%u%:00:00 /f 
-        schtasks /create /sc DAILY /tn s4 /tr "G:\My Drive\#Absolute pvt ltd\EVE\auto.bat" /st %u%:00:00 /f                                   
-    ) ELSE (
-        goto b
-    )
-) ELSE (
-    goto b
-)
-
-:b
+:ok4
 if %hour% GEQ %c% (
     if %hour% LSS %d% (
 
         push
         exit
     ) ELSE (
-        goto c
+        goto ok5
     )
 ) ELSE (
-    goto c
+    goto ok5
 )
 
-:c
+:ok5
 if %hour% GEQ %b% (
     if %hour% LSS %c% (
 
         push
         exit
     ) ELSE (
-        goto o
+        goto ok6
     )
 ) ELSE (
-    goto o
+    goto ok6
 )
 
-:o
+:ok6
 if %hour% GEQ %a% (
     if %hour% LSS %b% ( 
 
         push
         exit
     ) ELSE (
-        goto f
+        goto done
     )
 ) ELSE ( 
     push.bat
-    goto f
+    goto done
 )
-:f
+:done
 exit
--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "BO darbi" /min "%~f0" %* && exit
 git add --all
 git commit -m "%time%, %date%"
 git push -u origin main
